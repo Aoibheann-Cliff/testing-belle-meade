cbaf625b012749e162c2176beb4429e20a8102d4
Aoibheann Brady <aoibheann@cliff.studio>
2025-05-20 12:34:08 +0100

    Updated commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import "@/app/globals.css";
+import Head from 'next/head';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
+    
     <html lang="en">
+      <head>
+        <title>Belle Meade Village</title>
+      </head>
       <body>{children}</body>
     </html>
   );

2ebca1b525d41df746f92c4e9b2228bf0943a53d
Aoibheann Brady <aoibheann@cliff.studio>
2025-10-01 21:58:04 +0100

    Vercel Analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,32 +1,13 @@
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { SanityLive } from "@/sanity/lib/live";
-import Script from "next/script";
-import GATracker from "@/components/GATracker";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function FrontendLayout({ children }: { children: React.ReactNode }) {
   return (
     <section className="min-h-screen">
-      {/* GA scripts */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="ga-setup" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-
       <Header />
-
-      <GATracker /> {/* Only runs on client after hydration */}
-
+      <Analytics/>
       <main className="flex-grow">{children}</main>
       <Footer />
       <SanityLive />

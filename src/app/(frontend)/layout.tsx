644715a281fc14cc066e3bb5c8b2ed5bfe0c4be0
Aoibheann Brady <aoibheann@cliff.studio>
2025-10-16 17:02:32 +0100

    GA Test

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,11 +1,26 @@
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { SanityLive } from "@/sanity/lib/live";
+import Script from "next/script";
 
 export default function FrontendLayout({ children }: { children: React.ReactNode }) {
+  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
   return (
     <section className="min-h-screen">
       <Header />
+      {gaId && (
+          <>
+            <Script src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`} strategy="afterInteractive" />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaId}');
+              `}
+            </Script>
+          </>
+      )}
       <main className="flex-grow">{children}</main>
       <Footer />
       <SanityLive />
